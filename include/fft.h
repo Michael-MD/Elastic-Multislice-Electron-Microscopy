3d2f20e47618aa88021e151d5912948c46c68a67
mdei0001 <mdei0001@student.monash.edu>
2023-03-05 09:55:13 +1100

    added fft.h file

---
+++ include/fft.h
@@ -0,0 +1,11 @@
+// 1D FFT
+void rad2FFT(float xr[], float xi[], unsigned int N);
+void irad2FFT(float Xr[], float Xi[], unsigned int N);
+
+// 2D FFT
+void rad2FFT2(float **xr, float **xi, unsigned int Nx, unsigned int Ny);
+void irad2FFT2(float **Xr, float **Xi, unsigned int Nx, unsigned int Ny);
+
+//fftshift
+void fftshift(float xr[], float xi[], unsigned int N);
+void fftshift2(float **xr, float **xi, unsigned int Nx, unsigned int Ny);
