3be9816d74373d9e20dc006aa4fa781618f7511b
mdei0001 <mdei0001@student.monash.edu>
2023-03-25 10:40:22 +1100

    added clarifying comment

--- include/ABFSTEM.h
+++ include/ABFSTEM.h
@@ -6,6 +6,10 @@ using namespace std;
 
 class ABFSTEM : public ADFSTEM
 {
+	/*
+	Implemented Annular bright field STEM by inheriting ADFSTEM. This only serves as an alias for ADFSTEM
+	since ABF and ADF are the same except for the detector annular extent. 
+	*/
 public:
 	ABFSTEM(float E, int px_p, int py_p, int px, int py, int tx, int ty, int tz, string filename, vector<float> &s,
 		float Cs, float deltaf, float alpha_max, float alpha_min_D, float alpha_max_D);
